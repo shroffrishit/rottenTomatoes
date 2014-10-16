5846aa71cdb9af7564b1c7302e0b59763009d5e4
Rishit Shroff <rshroff@fb.com>
2014-10-15 22:35:19 -0700

    Initial Commit

---
+++ rottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rottenTomatoes
+//
+//  Created by Rishit Shroff on 10/15/14.
+//  Copyright (c) 2014 Rishit Shroff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
